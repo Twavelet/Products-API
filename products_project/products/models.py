3a166378ee9f9b7e895aecc9cbdb8de7d0c868b3
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-24 15:52:39 -0400

    Created properties in product app + initiated migrations to reflect these properties in MySQL

--- products_project/products/models.py
+++ products_project/products/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
