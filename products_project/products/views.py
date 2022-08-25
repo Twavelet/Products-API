19b6de6516032adb66b228eea657bf591e3bc588
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-25 11:28:30 -0400

    Created the functionality to delete a product form the Products database

--- products_project/products/views.py
+++ products_project/products/views.py
@@ -14,6 +14,7 @@ def products_list(request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
     elif request.method =='POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -21,15 +22,19 @@ def products_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'], ['PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
     elif request.method == 'PUT':
-        serializer = ProductSerializer(car, data=request.data)
+        serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)        
+        return Response(serializer.data)
 
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
